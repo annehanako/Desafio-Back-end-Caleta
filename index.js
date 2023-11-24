02dfe3d4b4b333087b12430bae1f5854b40866af
annehanako <anne.f.hanako@gmail.com>
2023-11-24 13:59:16 -0300

    balance and bet endpoint added

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = express ();
-server.get('/usuario', (req, res) => {
-    return res.json ({usuario: 'Api funcionando!'})
+const server = express();
+server.get('/user', (req, res) => {
+    return res.json({ user: 'API WORKING' })
 });
 
-server.listen (3000, () => {
-    console.log ("Server running on port 3000")
+server.listen(3000, () => {
+    console.log("Server running on port 3000")
 });
 
-server.use (bodyParser.json());
+server.use(bodyParser.json());
 
-// endpoints abaixo
+// endpoints
+
+let playerBalance = 1000; // player starting balance
+
+//endpoint to get player's balance
+server.get('/balance', (req, res) => {
+    res.json({ balance: playerBalance });
+});
+
+// endpoint to bet 
+server.post('/bet', (req, res) => {
+    const { amount } = req.body;
+
+    if (amount <= playerBalance){
+        playerBalance -= amount;
+        res.json ({ success: true, message: 'Your bet was sucessfully made.'});
+    } else {
+        res.status(400).json({ success: false, message:'Insuficient funds.'});
+    }  
+});
